1955c51b88e7bd8080b75bbc7d724dfaeed27158
Sayan Saha <sahasayan98@gmail.com>
2019-04-06 18:41:07 +0530

    receipt added

--- word_generation.py
+++ word_generation.py
@@ -1,22 +1,27 @@
+from __future__ import print_function
+from mailmerge import MailMerge
+from datetime import date
 import backend
 
-# from __future__ import print_function
-# from mailmerge import MailMerge
-# from datetime import date
 
-# template = "Reciepts/new.docx"
-
-# document = MailMerge(template)
-
-# document.merge(
-# 	name 	= 'Swaroop Acharjee',
-# 	amount 	= '500',
-# 	type 	= 'Registration Fees',
-# 	month	= 'April'
-# 	)
-
-# document.write('Reciepts/new2.docx')
 
 def generateCashMemo(StdId):
-	print(StdId)
+	template = "Reciepts/new.docx"
+
+	document = MailMerge(template)
+	for row in backend.searchData(StdId):
+		document.merge(
+			StdID 		= row[1],
+			Firstname 	= row[2],
+			Surname 	= row[3],
+			Dob 		= row[4],
+			Emailid		= row[5],
+			Subject		= row[6],
+			Address		= row[7],
+			Mobile		= row[8],
+			Amount 		= '1000',
+			Type 		= 'Registration'
+			)
+	
+	document.write('Reciepts/'+row[1]+row[2]+row[3]+'.docx')
 
